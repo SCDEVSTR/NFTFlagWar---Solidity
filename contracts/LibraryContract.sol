95d70e9013decec0d8f8a14cc458f1c6bfcb74c0
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-12-15 15:28:49 +0300

    Update LibraryContract.sol

--- contracts/LibraryContract.sol
+++ contracts/LibraryContract.sol
@@ -5,39 +5,48 @@ library LibraryContract  {
     struct Dot {
         //  gameID => Y index => X index => Dot
         //  mapping(uint256 => mapping(uint256 => mapping(uint256 => Dot))) public dots;
-
+        
         //Who is owner of flag
         address owner;
+        
         // Which is country of flag
         uint256 country;
+        
         // Which is last price of flag
         uint256 lastPrice;
     }
 
     struct Game {
-        // treasury will be distributed to winners
+
+        // Split every games accounting
+        // mapping(uint256 => LibraryContract.Game) public games;
+
+        // Treasury Amount
         uint256 treasury;
-        // the state of game
+        
+        // Game state
         State state;
-        // grid size
+        
+        // Grid x coordinate size
         uint256 xWidth;
-        // grid size
+        
+        // Grid y coordinate size
         uint256 yWidth;
-        // increase rate
+        
+        // Price minimum increase rate
         uint256 epsilon;
-        // every dot claim starts with this price
+        
+        // Game starting price
         uint256 claimBasePrice;
     }
 
     enum State {
-        // not started yet
+       
         Loading,
         Started,
         Paused,
         Resumed,
         Completed
     }
-
-
    
 }
